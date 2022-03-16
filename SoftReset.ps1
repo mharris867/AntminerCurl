298359d8571d125b82a7ab847e0c7d4fad27bff7
Mark McPhail <mmcphail@starbucks.com>
2022-03-16 03:07:55 -0700

    added color and ip input

--- SoftReset.ps1
+++ SoftReset.ps1
@@ -1,10 +1,12 @@
 $creds = Get-Credential # credentials are usually "root" user and "root" passowrd for Antminer
+$antminerIP = Read-Host -Prompt "Press enter to accept the default 192.168.2.27"
+if ([string]::IsNullOrWhiteSpace($antminerIP)){$antminerIP = "192.168.2.27"}
 $loopforever = $true
 While ($loopforever){
-    if ((Invoke-RestMethod http://192.168.2.27/cgi-bin/get_miner_status.cgi -Credential $creds -AllowUnencryptedAuthentication).summary.ghs5s -lt 27){
-        Invoke-RestMethod http://192.168.2.27/cgi-bin/reboot.cgi -Credential $creds -AllowUnencryptedAuthentication
-        Write-Host "$(Get-Date) Restarted Antminer 27"
+    if ((Invoke-RestMethod http://$antminerIP/cgi-bin/get_miner_status.cgi -Credential $creds -AllowUnencryptedAuthentication).summary.ghs5s -lt 27){
+        Invoke-RestMethod http://$antminerIP/cgi-bin/reboot.cgi -Credential $creds -AllowUnencryptedAuthentication
+        Write-Host "$(Get-Date) Restarted Antminer 27" -ForegroundColor DarkRed 
     }
-    Write-Host "$(Get-Date) Sleeping for 15min"
+    Write-Host "$(Get-Date) Sleeping for 15min" -ForegroundColor DarkGreen 
     Start-Sleep -Seconds 900
 }
