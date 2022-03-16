bba8b8158cac22b9d93f3f1eead40b12f0dd16c8
Mark McPhail <mmcphail@starbucks.com>
2022-03-16 03:13:53 -0700

    formatting changes and typo fix

--- SoftReset.ps1
+++ SoftReset.ps1
@@ -1,9 +1,9 @@
-$creds = Get-Credential # credentials are usually "root" user and "root" passowrd for Antminer
+$creds = Get-Credential # credentials are usually "root" user and "root" password for Antminer
 $antminerIP = Read-Host -Prompt "Press enter to accept the default 192.168.2.27"
-if ([string]::IsNullOrWhiteSpace($antminerIP)){$antminerIP = "192.168.2.27"}
+if ([string]::IsNullOrWhiteSpace($antminerIP)) { $antminerIP = "192.168.2.27" }
 $loopforever = $true
-While ($loopforever){
-    if ((Invoke-RestMethod http://$antminerIP/cgi-bin/get_miner_status.cgi -Credential $creds -AllowUnencryptedAuthentication).summary.ghs5s -lt 27){
+While ($loopforever) {
+    if ((Invoke-RestMethod http://$antminerIP/cgi-bin/get_miner_status.cgi -Credential $creds -AllowUnencryptedAuthentication).summary.ghs5s -lt 27) {
         Invoke-RestMethod http://$antminerIP/cgi-bin/reboot.cgi -Credential $creds -AllowUnencryptedAuthentication
         Write-Host "$(Get-Date) Restarted Antminer 27" -ForegroundColor DarkRed 
     }
