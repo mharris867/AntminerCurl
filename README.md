9cf56c0c66a9a58884c6dff153f28f9902647e20
Mark McPhail <mmcphail@starbucks.com>
2022-03-16 18:56:31 -0700

    figured out the payload for a soft reset and commented out all the work I did for a hard reset

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# AntminerCurl
-Antminer Curl Commands
+# AntminerLinuxPowershell
+One of the hash boards is going bad on my Antminer Z9 and a soft reset corrects the issue when it stops hashing. Instead of going in and fixing the board I attempted to automate the reset and learn powershell 7 on linux so that my script would be cross compatible with windows also. 
+Eventually I want to run this script on a Raspberry pi Zero on boot for a later project.
--- SoftReset.ps1
+++ SoftReset.ps1
@@ -1,13 +1,45 @@
-$creds = Get-Credential # credentials are usually "root" user and "root" password for Antminer
-$antminerIP = Read-Host -Prompt "Press enter to accept the default Antminer IP 192.168.2.27" # Prompt for Antminer IP
+if (!(Test-Path .\PSCreds_${env:USERNAME}.xml)) {
+    # look for stored credentials
+    Write-Host "Unable to find: $(Get-Location)/PSCreds_${env:USERNAME}.xml"
+    $creds = Get-Credential # credentials are usually "root" user and "root" password for Antminer
+    $creds | Export-CliXml -Path ".\PSCreds_${env:USERNAME}.xml" # Store credentials
+}
+else {
+    $creds = Import-CliXml -Path ".\PSCreds_${env:USERNAME}.xml" # Import Stored Credentials
+}
+if (!([bool]([Environment]::GetCommandLineArgs() -like '*-noninteractive*'))) {
+    # Do not prompt for user input if in noninteractive mode
+    $antminerUser = Read-Host -Prompt "Press enter to accept the default Antminer User mharris867.027" # Prompt for Antminer user
+    $antminerIP = Read-Host -Prompt "Press enter to accept the default Antminer IP 192.168.2.27" # Prompt for Antminer IP
+}
 if ([string]::IsNullOrWhiteSpace($antminerIP)) { $antminerIP = "192.168.2.27" } # Set Antminer IP to default if not input from user
+if ([string]::IsNullOrWhiteSpace($antminerUser)) { $antminerUser = "mharris867.027" } # Set Antminer User to default if not input from user
+
 $loopforever = $true
 While ($loopforever) {
-    # Starting with a Ksols/S(RT) setting of 27 Ksols in the line below to trigger a restart. This might need tweeking. 
+    # Starting with a Ksols/S(RT) setting of 32 Ksols in the line below to trigger a restart. This might need tweeking. 
     # Thinking about using http://@antminerIP/cgi-bin/get_miner_conf.cgi to find the bitmain-freq and increase or reduce the Ksols threshold if necessary
     if ((Invoke-RestMethod http://$antminerIP/cgi-bin/get_miner_status.cgi -Credential $creds -AllowUnencryptedAuthentication).summary.ghs5s -lt 32) {
-        Invoke-RestMethod http://$antminerIP/cgi-bin/reboot.cgi -Credential $creds -AllowUnencryptedAuthentication
-        Write-Host "$(Get-Date) Restarted Antminer 27" -ForegroundColor DarkRed 
+
+        # Commented out the try catch block for a hard restart. Everything works but the Soft Reset is easier on power supply
+        <# 
+        try {
+            Invoke-RestMethod http://$antminerIP/cgi-bin/reboot.cgi -Credential $creds -AllowUnencryptedAuthentication
+        }
+        Catch {
+            if (($Error[0] -like "*500 - Internal Server Error*")) {
+                Write-Warning "$(Get-Date) Lost connection to Antminer during restart and caused (500 - Internal Server Error). This is normal during reboot..." 
+            }
+            else {
+                Write-Error "$(Get-Date) $($Error[0])"
+            }
+        } 
+        #>
+
+        #url encoded body for soft reset. This is the only encoding I could get to work so far.
+        $body = "_ant_pool1url=stratum%2Btcp%3A%2F%2Fequihash.f2pool.com%3A5100&_ant_pool1user=$antminerUser&_ant_pool1pw=x&_ant_pool2url=stratum%2Btcp%3A%2F%2Fequihash.f2pool.com%3A5100&_ant_pool2user=$antminerUser&_ant_pool2pw=x&_ant_pool3url=&_ant_pool3user=&_ant_pool3pw=&_ant_nobeeper=false&_ant_notempoverctrl=false&_ant_fan_customize_switch=false&_ant_fan_customize_value=&_ant_freq=500"
+        Invoke-RestMethod http://$antminerIP/cgi-bin/set_miner_conf.cgi  -Credential $creds -AllowUnencryptedAuthentication -Method "POST" -Body $body -ContentType "application/x-www-form-urlencoded; charset=UTF-8" 
+        Write-Host "$(Get-Date) Slow hash rate detected. Restarted Antminer" -ForegroundColor DarkRed 
     }
     Write-Host "$(Get-Date) Sleeping for 15min" -ForegroundColor DarkGreen 
     Start-Sleep -Seconds 900
