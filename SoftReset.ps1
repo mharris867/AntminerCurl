40e6bcf397582651db57e2e47414c883aaaece2c
Mark McPhail <mmcphail@starbucks.com>
2022-03-17 11:37:28 -0700

    dynamically grabbing config data and removed user settings

--- SoftReset.ps1
+++ SoftReset.ps1
@@ -9,16 +9,15 @@ else {
 }
 if (!([bool]([Environment]::GetCommandLineArgs() -like '*-noninteractive*'))) {
     # Do not prompt for user input if in noninteractive mode
-    $antminerUser = Read-Host -Prompt "Press enter to accept the default Antminer User mharris867.027" # Prompt for Antminer user
     $antminerIP = Read-Host -Prompt "Press enter to accept the default Antminer IP 192.168.2.27" # Prompt for Antminer IP
 }
 if ([string]::IsNullOrWhiteSpace($antminerIP)) { $antminerIP = "192.168.2.27" } # Set Antminer IP to default if not input from user
-if ([string]::IsNullOrWhiteSpace($antminerUser)) { $antminerUser = "mharris867.027" } # Set Antminer User to default if not input from user
 
 $loopforever = $true
 While ($loopforever) {
     # Starting with a Ksols/S(RT) setting of 32 Ksols in the line below to trigger a restart. This might need tweeking. 
-    # Thinking about using http://@antminerIP/cgi-bin/get_miner_conf.cgi to find the bitmain-freq and increase or reduce the Ksols threshold if necessary
+    # Thinking about using http://$antminerIP/cgi-bin/get_miner_conf.cgi to find the bitmain-freq and increase or reduce the Ksols threshold if necessary
+    $minerconfig = Invoke-RestMethod http://$antminerIP/cgi-bin/get_miner_conf.cgi -Credential $creds -AllowUnencryptedAuthentication 
     if ((Invoke-RestMethod http://$antminerIP/cgi-bin/get_miner_status.cgi -Credential $creds -AllowUnencryptedAuthentication).summary.ghs5s -lt 32) {
 
         # Commented out the try catch block for a hard restart. Everything works but the Soft Reset is easier on power supply
@@ -35,9 +34,9 @@ While ($loopforever) {
             }
         } 
         #>
-
+        
         #url encoded body for soft reset. This is the only encoding I could get to work so far.
-        $body = "_ant_pool1url=stratum%2Btcp%3A%2F%2Fequihash.f2pool.com%3A5100&_ant_pool1user=$antminerUser&_ant_pool1pw=x&_ant_pool2url=stratum%2Btcp%3A%2F%2Fequihash.f2pool.com%3A5100&_ant_pool2user=$antminerUser&_ant_pool2pw=x&_ant_pool3url=&_ant_pool3user=&_ant_pool3pw=&_ant_nobeeper=false&_ant_notempoverctrl=false&_ant_fan_customize_switch=false&_ant_fan_customize_value=&_ant_freq=500"
+        $body = "_ant_pool1url=$([uri]::EscapeDataString($minerconfig.pools.url[0]))&_ant_pool1user=$($minerconfig.pools.user[0])&_ant_pool1pw=$($minerconfig.pools.pass[0])&_ant_pool2url=$([uri]::EscapeDataString($minerconfig.pools.url[1]))&_ant_pool2user=$($minerconfig.pools.user[1])&_ant_pool2pw=$($minerconfig.pools.pass[1])&_ant_pool3url=$([uri]::EscapeDataString($minerconfig.pools.url[2]))&_ant_pool3user=$($minerconfig.pools.user[2])&_ant_pool3pw=$($minerconfig.pools.pass[2])&_ant_nobeeper=false&_ant_notempoverctrl=false&_ant_fan_customize_switch=false&_ant_fan_customize_value=&_ant_freq=$($minerconfig.'bitmain-freq')"
         Invoke-RestMethod http://$antminerIP/cgi-bin/set_miner_conf.cgi  -Credential $creds -AllowUnencryptedAuthentication -Method "POST" -Body $body -ContentType "application/x-www-form-urlencoded; charset=UTF-8" 
         Write-Host "$(Get-Date) Slow hash rate detected. Restarted Antminer" -ForegroundColor DarkRed 
     }
